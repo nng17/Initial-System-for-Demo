1e7faed826af80317577bf6f2732b02b95f20443
nng17 <nng@scu.edu>
2016-10-31 11:34:28 -0700

    Update html_to_csv.js

--- html_to_csv.js
+++ html_to_csv.js
@@ -1,74 +1,56 @@
-jQuery.fn.table2CSV = function(options) {
-    var options = jQuery.extend({
-        separator: ',',
-        header: [],
-        delivery: 'popup' // popup, value
-    },
-    options);
+$(document).ready(function () {
 
-    var csvData = [];
-    var headerArr = [];
-    var el = this;
-
-    //header
-    var numCols = options.header.length;
-    var tmpRow = []; // construct header avalible array
-
-    if (numCols > 0) {
-        for (var i = 0; i < numCols; i++) {
-            tmpRow[tmpRow.length] = formatData(options.header[i]);
-        }
-    } else {
-        $(el).filter(':visible').find('th').each(function() {
-            if ($(this).css('display') != 'none') tmpRow[tmpRow.length] = formatData($(this).html());
-        });
+	function exportTableToCSV($table, filename) {
+    
+        var $rows = $table.find('tr:has(td),tr:has(th)'),
+    
+            // Temporary delimiter characters unlikely to be typed by keyboard
+            // This is to avoid accidentally splitting the actual contents
+            tmpColDelim = String.fromCharCode(11), // vertical tab character
+            tmpRowDelim = String.fromCharCode(0), // null character
+    
+            // actual delimiter characters for CSV format
+            colDelim = '","',
+            rowDelim = '"\r\n"',
+    
+            // Grab text from table into CSV formatted string
+            csv = '"' + $rows.map(function (i, row) {
+                var $row = $(row), $cols = $row.find('td,th');
+    
+                return $cols.map(function (j, col) {
+                    var $col = $(col), text = $col.text();
+    
+                    return text.replace(/"/g, '""'); // escape double quotes
+    
+                }).get().join(tmpColDelim);
+    
+            }).get().join(tmpRowDelim)
+                .split(tmpRowDelim).join(rowDelim)
+                .split(tmpColDelim).join(colDelim) + '"',
+    
+            
+    
+            // Data URI
+            csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+            
+            console.log(csv);
+            
+        	if (window.navigator.msSaveBlob) { // IE 10+
+        		//alert('IE' + csv);
+        		window.navigator.msSaveOrOpenBlob(new Blob([csv], {type: "text/plain;charset=utf-8;"}), "csvname.csv")
+        	} 
+        	else {
+        		$(this).attr({ 'download': filename, 'href': csvData, 'target': '_blank' }); 
+        	}
     }
-
-    row2CSV(tmpRow);
-
-    // actual data
-    $(el).find('tr').each(function() {
-        var tmpRow = [];
-        $(this).filter(':visible').find('td').each(function() {
-            if ($(this).css('display') != 'none') tmpRow[tmpRow.length] = formatData($(this).html());
-        });
-        row2CSV(tmpRow);
+    
+    // This must be a hyperlink
+    $("#xx").on('click', function (event) {
+    	
+        exportTableToCSV.apply(this, [$('#projectSpreadsheet'), 'export.csv']);
+        
+        // IF CSV, don't do event.preventDefault() or return false
+        // We actually need this to be a typical hyperlink
     });
-    if (options.delivery == 'popup') {
-        var mydata = csvData.join('\n');
-        return popup(mydata);
-    } else {
-        var mydata = csvData.join('\n');
-        return mydata;
-    }
 
-    function row2CSV(tmpRow) {
-        var tmp = tmpRow.join('') // to remove any blank rows
-        // alert(tmp);
-        if (tmpRow.length > 0 && tmp != '') {
-            var mystr = tmpRow.join(options.separator);
-            csvData[csvData.length] = mystr;
-        }
-    }
-    function formatData(input) {
-        // replace " with “
-        var regexp = new RegExp(/["]/g);
-        var output = input.replace(regexp, "“");
-        //HTML
-        var regexp = new RegExp(/\<[^\<]+\>/g);
-        var output = output.replace(regexp, "");
-        if (output == "") return '';
-        return '"' + output + '"';
-    }
-    function popup(data) {
-        var generator = window.open('', 'csv', 'height=400,width=600');
-        generator.document.write('<html><head><title>CSV</title>');
-        generator.document.write('</head><body >');
-        generator.document.write('<textArea cols=70 rows=15 wrap="off" >');
-        generator.document.write(data);
-        generator.document.write('</textArea>');
-        generator.document.write('</body></html>');
-        generator.document.close();
-        return true;
-    }
-};
+});
