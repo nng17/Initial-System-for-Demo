5120ef6e7b183ec3533a4df36a13974585b59a8a
nng17 <nng@scu.edu>
2016-10-31 18:25:19 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
 function changeText(id) {
 	console.log($("#"+id));
-	var oldtext = $("#"+id).html();
-	console.log(oldtext); 
-	if (oldtext == "YES") {
+	//var oldtext = $("#"+id).html();
+	if ($("#"+id).html == "YES") {
 		$("#" + id).html("NO");
 	}
 	else {
